3009d7876ee09cec7072da9826774c6af3ff4e04
Víctor Manuel Rivas Santos <vrivas@vrivas.es>
2024-11-28 16:52:17 +0100

    Create instalar-moodle-bitnami-docker.sh

---
+++ instalar-moodle-bitnami-docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# PASO 1: Obtener imagen 
+docker pull bitnami/moodle:latest
+
+# PASO 2: Crear red 
+docker network create moodle-network
+
+# PASO 3: Crear volumen para BBDD MariaDB
+docker volume create --name mariadb_data
+
+# PASO 4: Ejecutar contenedor para BBDD MariaDB
+docker run -d --name mariadb \
+  --env ALLOW_EMPTY_PASSWORD=yes \
+  --env MARIADB_USER=bn_moodle \
+  --env MARIADB_PASSWORD=bitnami \
+  --env MARIADB_DATABASE=bitnami_moodle \
+  --network moodle-network \
+  --volume mariadb_data:/bitnami/mariadb \
+  bitnami/mariadb:latest
+
+# PASO 5: Crear volumen para Moodle
+docker volume create --name moodle_data
+
+# PASO 6: Ejecutar contenedor para Moodle
+docker run -d --name moodle \
+  -p 8080:8080 -p 8443:8443 \
+  --env ALLOW_EMPTY_PASSWORD=yes \
+  --env MOODLE_DATABASE_USER=bn_moodle \
+  --env MOODLE_DATABASE_PASSWORD=bitnami \
+  --env MOODLE_DATABASE_NAME=bitnami_moodle \
+  --network moodle-network \
+  --volume moodle_data:/bitnami/moodle \
+  --volume moodledata_data:/bitnami/moodledata \
+  bitnami/moodle:latest
+
