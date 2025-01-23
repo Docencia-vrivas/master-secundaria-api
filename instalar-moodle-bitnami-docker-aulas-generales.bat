1087bcf593b60f9ba82de9fcba153e4c0d2a0b70
Víctor Manuel Rivas Santos <vrivas@ujaen.es>
2025-01-23 13:11:38 +0100

    Script probado en el aula I13

--- instalar-moodle-bitnami-docker-aulas-generales.bat
+++ instalar-moodle-bitnami-docker-aulas-generales.bat
@@ -6,8 +6,7 @@ echo "Paso 2: Crear red"
 docker network create moodle-network
 
 echo "Paso 3: Crear volumen para BBDD MariaDB"
-# Asumimos que quieres crear un volumen en D:\datos_mariadb
-docker volume create --name mariadb_data -o driver=local -o type=none -o device=D:\datos_mariadb
+docker volume create mariadb_data
 
 echo "Paso 4: Ejecutar contenedor para BBDD MariaDB"
 docker run -d --name mariadb ^
@@ -16,12 +15,11 @@ docker run -d --name mariadb ^
   --env MARIADB_PASSWORD=bitnami ^
   --env MARIADB_DATABASE=bitnami_moodle ^
   --network moodle-network ^
-  --volume mariadb_data:/bitnami/mariadb ^
+  --volume /d/mariadb_data:/bitnami/mariadb ^
   bitnami/mariadb:latest
 
 echo "Paso 5: Crear volumen para Moodle"
-# Asumimos que quieres crear un volumen en D:\datos_moodle
-docker volume create --name moodle_data -o driver=local -o type=none -o device=D:\datos_moodle
+docker volume create moodle_data
 
 echo "Paso 6: Ejecutar contenedor para Moodle"
 docker run -d --name moodle ^
@@ -31,8 +29,8 @@ docker run -d --name moodle ^
   --env MOODLE_DATABASE_PASSWORD=bitnami ^
   --env MOODLE_DATABASE_NAME=bitnami_moodle ^
   --network moodle-network ^
-  --volume moodle_data:/bitnami/moodle ^
-  --volume moodledata_data:/bitnami/moodledata ^
+  --volume /d/moodle_data:/bitnami/moodle ^
+  --volume /d/moodledata_data:/bitnami/moodledata ^
   bitnami/moodle:latest
 
 echo "Proceso completado."
